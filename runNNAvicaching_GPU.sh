257ae9595ae1b6aca4b42d427e2bebd556585c50
anmolkabra <ak2426@cornell.edu>
2017-06-14 18:22:16 -0400

    testing if the model can find the weights used for randXYR

--- runNNAvicaching_GPU.sh
+++ runNNAvicaching_GPU.sh
@@ -3,6 +3,6 @@ for e in 200 400 600
 do
     for lr in 0.1 0.01
     do
-        python nnAvicaching.py --lr $lr --epochs $e --rand-xyr --save-plot
+        python nnAvicaching.py --lr $lr --epochs $e --train-percent 1.0 --rand-xyr --save-plot
     done
 done

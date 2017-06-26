a3a25cd3d2c5e84134ed41afa66a9cf7bb40fee3
anmolkabra <ak2426@cornell.edu>
2017-06-26 10:46:54 -0400

    logging gpu status and cleaned up before running

--- runNNAvicaching_find_weights.sh
+++ runNNAvicaching_find_weights.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+nvidia-smi > ./stats/before_find_weights_gpu_status.txt
 python nnAvicaching.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 1
+nvidia-smi > ./stats/running_find_weights_gpu_status.txt
 python nnAvicaching.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 2
 python nnAvicaching.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 3
 python nnAvicaching.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 1 --no-cuda
---
+++ stats/find_rewards/README.md
@@ -0,0 +1,1 @@
+## stats of find_rewards exec

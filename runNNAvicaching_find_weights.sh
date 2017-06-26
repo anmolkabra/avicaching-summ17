5ae08baf5b37b4541bb7068936f243c7aecdd540
anmolkabra <ak2426@cornell.edu>
2017-06-26 10:52:20 -0400

    wrong file name

--- runNNAvicaching_find_weights.sh
+++ runNNAvicaching_find_weights.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 nvidia-smi > ./stats/before_find_weights_gpu_status.txt
-python nnAvicaching.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 1
+python nnAvicaching_find_weights.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 1
 nvidia-smi > ./stats/running_find_weights_gpu_status.txt
-python nnAvicaching.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 2
-python nnAvicaching.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 3
-python nnAvicaching.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 1 --no-cuda
-python nnAvicaching.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 2 --no-cuda
-python nnAvicaching.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 3 --no-cuda
+python nnAvicaching_find_weights.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 2
+python nnAvicaching_find_weights.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 3
+python nnAvicaching_find_weights.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 1 --no-cuda
+python nnAvicaching_find_weights.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 2 --no-cuda
+python nnAvicaching_find_weights.py --epochs 10000 --hide-loss-plot --hide-map-plot --seed 3 --no-cuda

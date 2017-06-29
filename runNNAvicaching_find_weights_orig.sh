7fa203ee1d9165ec9faa9609543d08b022b6d2f7
anmolkabra <ak2426@cornell.edu>
2017-06-29 18:18:34 -0400

    tested orig data with 2 sets of weights

--- runNNAvicaching_find_weights_orig.sh
+++ runNNAvicaching_find_weights_orig.sh
@@ -2,6 +2,5 @@
 python nnAvicaching_find_weights.py --hide-map-plot --hide-loss-plot --epochs 10000 --seed 1
 python nnAvicaching_find_weights.py --hide-map-plot --hide-loss-plot --epochs 10000 --seed 2
 python nnAvicaching_find_weights.py --hide-map-plot --hide-loss-plot --epochs 10000 --seed 3
-python nnAvicaching_find_weights.py --hide-map-plot --hide-loss-plot --epochs 10000 --seed 1 --no-cuda
-python nnAvicaching_find_weights.py --hide-map-plot --hide-loss-plot --epochs 10000 --seed 2 --no-cuda
-python nnAvicaching_find_weights.py --hide-map-plot --hide-loss-plot --epochs 10000 --seed 3 --no-cuda
+python nnAvicaching_find_weights.py --hide-map-plot --hide-loss-plot --epochs 10000 --seed 4
+python nnAvicaching_find_weights.py --hide-map-plot --hide-loss-plot --epochs 10000 --seed 5

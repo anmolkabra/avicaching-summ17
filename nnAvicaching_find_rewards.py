c97f685e128ecfa74ad6705da797431eaffa8f28
anmolkabra <ak2426@cornell.edu>
2017-06-30 12:13:43 -0400

    corrected rewards script to support 2 sets of weights, tested

--- nnAvicaching_find_rewards.py
+++ nnAvicaching_find_rewards.py
@@ -58,7 +58,7 @@ if args.cuda:
 # parameters and constants
 # =============================================================================
 J, T, weights_file_name, totalR = args.locations, args.time, args.weights_file, args.rewards
-X, W_for_r, F_DIST_weighted, numFeatures = [], [], [], 0
+X, w1_for_r, w2, F_DIST_w1, numFeatures = [], [], [], [], 0
 torchten = torch.FloatTensor
 lp_A, lp_c = [], []
 
@@ -70,7 +70,7 @@ randDIST_file = "./data/random/randDIST" + str(J) + ".txt"
 # data input
 # =============================================================================
 def read_set_data():
-    global X, numFeatures, F_DIST_weighted, W_for_r
+    global X, numFeatures, F_DIST_w1, w1_for_r, w2
     # read f and dist datasets from file, operate on them
     if args.rand:
         F = ad.read_F_file(randF_file, J)
@@ -79,28 +79,32 @@ def read_set_data():
         F = ad.read_F_file("./data/loc_feature_with_avicaching_combined.csv", J)
         DIST = ad.read_dist_file("./data/site_distances_km_drastic_price_histlong_0327_0813_combined.txt", J)
 
-    # read W and X
-    w1, w2 = ad.read_weights_file(weights_file_name, J, numFeatures)
-    if args.rand:
-        X, _, _ = ad.read_XYR_file(randXYR_file, J, T)
-    else:
-        X, _, _ = ad.read_XYR_file("./data/density_shift_histlong_as_previous_loc_classical_drastic_price_0327_0813.txt", J, T)
-
     # process data for the NN
     F, DIST = ad.normalize(F, along_dim=0, using_max=True), ad.normalize(DIST, using_max=True)  # normalize using max
     numFeatures = len(F[0]) + 1     # distance included
     F_DIST = torchten(ad.combine_DIST_F(F, DIST, J, numFeatures))
     numFeatures += 1                # for rewards later
+
+    # read W and X
+    w1, w2 = ad.read_weights_file(weights_file_name, J, numFeatures)
+    w2 = np.expand_dims(w2, axis=2)
+
+    if args.rand:
+        X, _, _ = ad.read_XYR_file(randXYR_file, J, T)
+    else:
+        X, _, _ = ad.read_XYR_file("./data/density_shift_histlong_as_previous_loc_classical_drastic_price_0327_0813.txt", J, T)
     
-    # split W and join the multiply the fdist portion with F_DIST
-    W = np.expand_dims(W, axis=2)
-    W_for_fdist, W_for_r = ad.split_along_dim(W, numFeatures - 1, dim=1)
-    F_DIST_weighted = Variable(torch.bmm(F_DIST, torchten(W_for_fdist)).squeeze(dim=2), requires_grad=False)
+    # split w1; multiply the fdist portion of w1 with F_DIST
+    w1_for_fdist, w1_for_r = ad.split_along_dim(w1, numFeatures - 1, dim=1)
+    F_DIST_w1 = Variable(torch.bmm(F_DIST, torchten(w1_for_fdist)).squeeze(dim=2), requires_grad=False)
 
     # condense X along T into a single vector and normalize
     X = ad.normalize(X.sum(axis=0), using_max=False)
     
-    W_for_r, X = Variable(torchten(W_for_r), requires_grad=False), Variable(torchten(X), requires_grad=False)
+    w1_for_r, X = Variable(torchten(w1_for_r), requires_grad=False), Variable(torchten(X), requires_grad=False)
+    w2 = Variable(torchten(w2), requires_grad=False)
+    # w1_for_r shape J x 1 x numF
+    # w2 shape J x numF x 1
 
 # =============================================================================
 # MyNet class
@@ -114,23 +118,24 @@ class MyNet(nn.Module):
         normalizedR = ad.normalize(self.r, using_max=False)
         self.R = nn.Parameter(torchten(normalizedR))
 
-    def forward(self, inp):
-        repeatedR = self.R.repeat(J, 1).unsqueeze(dim=2)
-        inp = torch.bmm(repeatedR, W_for_r).view(-1, J) + F_DIST_weighted
-        inp = torchfun.relu(inp)
+    def forward(self, wt1, wt2):
+        repeatedR = self.R.repeat(J, 1).unsqueeze(dim=2)    # shape is J x J x 1
+        res = torch.bmm(repeatedR, wt1) + F_DIST_w1     # inp is J x J x numF after
+        res = torchfun.relu(res)
+        res = torch.bmm(res, wt2).view(-1, J)    # inp is J x J
         # add eta to inp[u][u]
         # eta_matrix = Variable(eta * torch.eye(J).type(torchten))
         # if args.cuda:
         #    eta_matrix = eta_matrix.cuda()
         # inp += eta_matrix
-        return torchfun.softmax(inp)
+        return torchfun.softmax(res)
 
 def train(net, optimizer):
-    global W_for_r, lp_A, lp_c
+    global w1_for_r, w2, lp_A, lp_c
     start_train = time.time()
 
     # feed in data
-    P = net(W_for_r).t()    # P is now weighted -> softmax
+    P = net(w1_for_r, w2).t()    # P is now weighted -> softmax
     
     # calculate loss
     Y = torch.mv(P, X)
@@ -144,8 +149,10 @@ def train(net, optimizer):
     optimizer.step()
     end_train = time.time() - start_train
 
-    r_on_cpu = net.R.data.squeeze().cpu().numpy()   # transfer data
+    r_on_cpu = net.R.data.squeeze().cpu().numpy()   # transfer data for lp
     start_lp = time.time()
+    # 1.0 is the sum constraint of rewards
+    # the first J outputs are the new rewards
     net.R.data = torchten(lp.run_lp(lp_A, lp_c, J, r_on_cpu, 1.0).x[:J]).unsqueeze(dim=0)
     end_lp = time.time() - start_lp
     if args.cuda:
@@ -155,13 +162,14 @@ def train(net, optimizer):
     return (end_train + end_lp, loss.data[0], net.R.data.sum())
 
 def test_rewards(r):
-    global W_for_r
+    global w1_for_r, w2
     start_time = time.time()
 
     # feed in data
     repeatedR = r.repeat(J, 1).unsqueeze(dim=2)
-    inp = torch.bmm(repeatedR, W_for_r).view(-1, J) + F_DIST_weighted
+    inp = torch.bmm(repeatedR, w1_for_r) + F_DIST_w1
     inp = torchfun.relu(inp)
+    inp = torch.bmm(inp, w2).view(-1, J)
     # add eta to inp[u][u]
     # eta_matrix = Variable(eta * torch.eye(J).type(torchten))
     # inp += eta_matrix
@@ -226,7 +234,7 @@ if __name__ == "__main__":
     net = MyNet()
     if args.cuda:
         net.cuda()
-        W_for_r, F_DIST_weighted, X = W_for_r.cuda(), F_DIST_weighted.cuda(), X.cuda()
+        w1_for_r, w2, F_DIST_w1, X = w1_for_r.cuda(), w2.cuda(), F_DIST_w1.cuda(), X.cuda()
         file_pre_gpu = "gpu, "
     else:
         file_pre_gpu = "cpu, "
@@ -250,8 +258,8 @@ if __name__ == "__main__":
         file_pre = "randXYR_seed=%d, epochs=%d, " % (args.seed, args.epochs)
     else:
         file_pre = "origXYR_seed=%d, epochs=%d, " % (args.seed, args.epochs)
-    log_name = "lr=%.3e, mom=%.3e, time=%.4f sec" % (
-        args.lr, args.momentum, total_time)
+    log_name = "lr=%.3e, time=%.4f sec" % (
+        args.lr, total_time)
     epoch_data = np.arange(1, args.epochs + 1)
     fname = file_pre_gpu + file_pre + log_name
 
--- runNNAvicaching_find_rewards.sh
+++ runNNAvicaching_find_rewards.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-python nnAvicaching_find_rewards.py --seed 1 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=1, epochs=10000, train= 80%, lr=1.000e-02, time=981.7946 sec.txt"
-python nnAvicaching_find_rewards.py --seed 2 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-02, time=983.9379 sec.txt"
-python nnAvicaching_find_rewards.py --seed 3 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=3, epochs=10000, train= 80%, lr=1.000e-02, time=975.6399 sec.txt"
-python nnAvicaching_find_rewards.py --seed 1 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=1, epochs=10000, train= 80%, lr=1.000e-02, time=981.7946 sec.txt" --no-cuda
-python nnAvicaching_find_rewards.py --seed 2 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-02, time=983.9379 sec.txt" --no-cuda
-python nnAvicaching_find_rewards.py --seed 3 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=3, epochs=10000, train= 80%, lr=1.000e-02, time=975.6399 sec.txt" --no-cuda
+python nnAvicaching_find_rewards.py --seed 1 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=1, epochs=10000, train= 80%, lr=1.000e-02, time=1260.1639 sec.txt"
+python nnAvicaching_find_rewards.py --seed 2 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-02, time=1262.2267 sec.txt"
+python nnAvicaching_find_rewards.py --seed 3 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=3, epochs=10000, train= 80%, lr=1.000e-02, time=1261.1577 sec.txt"
+python nnAvicaching_find_rewards.py --seed 4 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=4, epochs=10000, train= 80%, lr=1.000e-02, time=1259.9575 sec.txt"
+python nnAvicaching_find_rewards.py --seed 5 --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=5, epochs=10000, train= 80%, lr=1.000e-02, time=1259.5761 sec.txt"

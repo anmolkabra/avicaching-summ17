2225cd0ea95a01d3a21a5fc513835873c2d6fa48
anmolkabra <ak2426@cornell.edu>
2017-07-03 11:18:57 -0400

    testing weights with T=85

--- runNNAvicaching_find_weights_rand.sh
+++ runNNAvicaching_find_weights_rand.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 1
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 2
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 3
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 1 --no-cuda
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 2 --no-cuda
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 3 --no-cuda
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 1
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 2
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 3
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 1 --no-cuda
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 2 --no-cuda
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 3 --no-cuda
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 1
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 2
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 3
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 1 --no-cuda
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 2 --no-cuda
-python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 3 --no-cuda
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 1
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 2
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 3
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 1 --no-cuda
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 2 --no-cuda
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 17 --seed 3 --no-cuda
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 1
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 2
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 3
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 1 --no-cuda
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 2 --no-cuda
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 173 --seed 3 --no-cuda
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 1
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 2
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 3
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 1 --no-cuda
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 2 --no-cuda
+# python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 1730 --seed 3 --no-cuda
+python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 85 --seed 1
+python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 85 --seed 2
+python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 85 --seed 3
+python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 85 --seed 1 --no-cuda
+python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 85 --seed 2 --no-cuda
+python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time 85 --seed 3 --no-cuda

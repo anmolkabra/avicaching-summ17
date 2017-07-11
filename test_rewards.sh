119185f5ed34f9ee36d8b1d492c6a2495d71875c
anmolkabra <ak2426@cornell.edu>
2017-07-11 15:31:18 -0400

    writing optimizing reward section, stats, modified testing file

--- test_rewards.sh
+++ test_rewards.sh
@@ -1,25 +1,8 @@
 #!/usr/bin/env bash
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/proportionateRewards_7-1_1747.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=1, epochs=10000, train= 80%, lr=1.000e-02, time=1260.1639 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/proportionateRewards_7-1_1747.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-02, time=1262.2267 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/proportionateRewards_7-1_1747.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=3, epochs=10000, train= 80%, lr=1.000e-02, time=1261.1577 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/proportionateRewards_7-1_1747.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=4, epochs=10000, train= 80%, lr=1.000e-02, time=1259.9575 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/proportionateRewards_7-1_1747.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=5, epochs=10000, train= 80%, lr=1.000e-02, time=1259.5761 sec.txt"
+python nnAvicaching_find_rewards.py --test "./stats/find_rewards/proportionateRewards_7-1_1747.txt" --weights-file "./stats/find_weights/weights/orig/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-03, time=1157.1230 sec.txt"
 
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1109.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=1, epochs=10000, train= 80%, lr=1.000e-02, time=1260.1639 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1109.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-02, time=1262.2267 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1109.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=3, epochs=10000, train= 80%, lr=1.000e-02, time=1261.1577 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1109.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=4, epochs=10000, train= 80%, lr=1.000e-02, time=1259.9575 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1109.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=5, epochs=10000, train= 80%, lr=1.000e-02, time=1259.5761 sec.txt"
+python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1109.txt" --weights-file "./stats/find_weights/weights/orig/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-03, time=1157.1230 sec.txt"
 
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1203.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=1, epochs=10000, train= 80%, lr=1.000e-02, time=1260.1639 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1203.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-02, time=1262.2267 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1203.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=3, epochs=10000, train= 80%, lr=1.000e-02, time=1261.1577 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1203.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=4, epochs=10000, train= 80%, lr=1.000e-02, time=1259.9575 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1203.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=5, epochs=10000, train= 80%, lr=1.000e-02, time=1259.5761 sec.txt"
+python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_6-29_1203.txt" --weights-file "./stats/find_weights/weights/orig/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-03, time=1157.1230 sec.txt"
 
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_7-3_1048.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=1, epochs=10000, train= 80%, lr=1.000e-02, time=1260.1639 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_7-3_1048.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-02, time=1262.2267 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_7-3_1048.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=3, epochs=10000, train= 80%, lr=1.000e-02, time=1261.1577 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_7-3_1048.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=4, epochs=10000, train= 80%, lr=1.000e-02, time=1259.9575 sec.txt"
-python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_7-3_1048.txt" --weights-file "./stats/find_weights/weights/gpu, origXYR_seed=5, epochs=10000, train= 80%, lr=1.000e-02, time=1259.5761 sec.txt"
-
+python nnAvicaching_find_rewards.py --test "./stats/find_rewards/randomRewards_sum_1000_7-3_1048.txt" --weights-file "./stats/find_weights/weights/orig/gpu, origXYR_seed=2, epochs=10000, train= 80%, lr=1.000e-03, time=1157.1230 sec.txt"

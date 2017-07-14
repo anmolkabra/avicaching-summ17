c693c3b091ce34fa4197bbeeeefcabe252404481
anmolkabra <ak2426@cornell.edu>
2017-07-14 15:07:28 -0400

    new weights file

--- runNNAvicaching_find_rewards_rand.sh
+++ runNNAvicaching_find_rewards_rand.sh
@@ -10,7 +10,7 @@ for J in 11 35 55 85 116
 do
     for s in 1 2 3
     do
-        python nnAvicaching_find_rewards.py --rand --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/rand/gpu, randXYR_seed=1, epochs=1000, train= 80%, lr=1.000e-02, time=126.0350 sec.txt" --locations $J --seed $s
-        python nnAvicaching_find_rewards.py --rand --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/rand/gpu, randXYR_seed=1, epochs=1000, train= 80%, lr=1.000e-02, time=126.0350 sec.txt" --locations $J --seed $s --no-cuda
+        python nnAvicaching_find_rewards.py --rand --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/rand/gpu, randXYR_seed=1, epochs=1000, train= 80%, lr=1.000e-03, time=119.2222 sec.txt" --locations $J --seed $s
+        python nnAvicaching_find_rewards.py --rand --epochs 1000 --hide-loss-plot --weights-file "./stats/find_weights/weights/rand/gpu, randXYR_seed=1, epochs=1000, train= 80%, lr=1.000e-03, time=119.2222 sec.txt" --locations $J --seed $s --no-cuda
     done
 done

54e9e0bc3126179b7b5e35f750a72322b4d50041
anmolkabra <ak2426@cornell.edu>
2017-07-19 13:26:27 -0400

    commented out temp stuff

--- log_ram_usage_rewards.sh
+++ log_ram_usage_rewards.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # only lp
-echo "pid, cpu, mem, process" > ./stats/onlylp.txt
-wait $(jobs -p)
-python test_lp_time.py --epochs 200 &
-while [ `pgrep python` ]
-do
-    sleep .2
-    ps aux | awk '{print $2, $3, $4, $11}' | sort -k2rn | head -n 3 >> ./stats/onlylp.txt && echo "----" >> ./stats/onlylp.txt
-    echo "onlylp"
-done
-exit
+# echo "pid, cpu, mem, process" > ./stats/onlylp.txt
+# wait $(jobs -p)
+# python test_lp_time.py --epochs 200 &
+# while [ `pgrep python` ]
+# do
+#     sleep .2
+#     ps aux | awk '{print $2, $3, $4, $11}' | sort -k2rn | head -n 3 >> ./stats/onlylp.txt && echo "----" >> ./stats/onlylp.txt
+#     echo "onlylp"
+# done
+# exit
 
 # gpu set
 echo "pid, cpu, mem, process" > ./stats/cpu_gpuset.txt

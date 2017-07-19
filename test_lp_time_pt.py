e2177e26b36c15f66d07fc30e29341a330ab90c9
anmolkabra <ak2426@cornell.edu>
2017-07-19 13:25:04 -0400

    logging data like normal test_lp_time but not testing

--- test_lp_time_pt.py
+++ test_lp_time_pt.py
@@ -35,6 +35,25 @@ if args.cuda:
 J=116
 lp_A, lp_c = lp.build_A(J), lp.build_c(J)
 
+###### ONLY LP
+# lp_A, lp_c = lp.build_A(J), lp.build_c(J)
+# lp_time_log = []
+# for e in xrange(args.epochs):
+#     r_on_cpu = np.random.randn(J)
+#     start_lp_time = time.time()
+# 
+#     # CONSTRAIN -- LP
+#     # 1.0 is the sum constraint of rewards
+#     # the first J outputs are the new rewards
+#     lp_res = lp.run_lp(lp_A, lp_c, J, r_on_cpu, 1.0)
+#     lp_time = time.time() - start_lp_time
+#     print(lp_time)
+#     lp_time_log.append([e, lp_time])
+# fname = "pt_onlylp, epochs=%d, time=%.0f" % (args.epochs, time.time())
+# np.savetxt("./stats/" + fname + ".txt", lp_time_log, fmt="%.6f", delimiter=",")
+# sys.exit()
+###########
+
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=True, download=True,
@@ -87,16 +106,18 @@ def train(epoch):
         loss.backward()
         optimizer.step()
 
-        ### lp run
-        r_on_cpu = np.random.randn(J)
-        start_lp_time = time.time()
-        lp_res = lp.run_lp(lp_A, lp_c, J, r_on_cpu, 1.0)
-        print(time.time() - start_lp_time)
+    ### lp run
+    r_on_cpu = np.random.randn(J)
+    start_lp_time = time.time()
+    lp_res = lp.run_lp(lp_A, lp_c, J, r_on_cpu, 1.0)
+    lp_time = time.time() - start_lp_time
+    print(lp_time)
 
         # if batch_idx % args.log_interval == 0:
         #     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
         #         epoch, batch_idx * len(data), len(train_loader.dataset),
         #         100. * batch_idx / len(train_loader), loss.data[0]))
+    return lp_time
 
 def test():
     model.eval()
@@ -116,7 +137,14 @@ def test():
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
-
+lp_time_log = []
 for epoch in range(1, args.epochs + 1):
-    print(epoch)
-    train(epoch)
+    lp_t = train(epoch)
+    lp_time_log.append([epoch, lp_t])
+
+if args.cuda:
+    file_pre = "gpu, "
+else:
+    file_pre = "cpu, "
+fname = "epochs=%d, time=%.0f" % (args.epochs, time.time())
+np.savetxt("./stats/pt_" + file_pre + fname + ".txt", lp_time_log, fmt="%.6f", delimiter=",")

dfab55463607e68f76224f38ee82120178c1e411
anmolkabra <ak2426@cornell.edu>
2017-07-26 12:34:40 -0400

    will test the speed of half precision vs single precision

---
+++ half_vs_float_weights.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# testing GPU "set" with half and float tensors
+# specs for tests:
+# locations: 116
+# batch-size T: 17, 51, 85, 129, 173
+# lr: 0.001
+# epochs: 1000
+
+for T in 17 51 85 129 173
+do
+    for s in 1 2 3
+    do
+        python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time $T --seed $s
+        sed -i 's/FloatTensor/HalfTensor/g' nnAvicaching_find_weights.py
+        python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time $T --seed $s
+        sed -i 's/HalfTensor/FloatTensor/g' nnAvicaching_find_weights.py
+    done
+done
+
+python nnAvicaching_find_weights.py --no-plots --epochs 10000 --seed 1 --lr 0.001

358fedf105174473e795f8fde8483decdb242a0b
anmolkabra <ak2426@cornell.edu>
2017-07-31 11:10:00 -0400

    documentation and changing run specs for weights rand. batch-size changing with J instead of T

--- avicaching_data.py
+++ avicaching_data.py
@@ -1,11 +1,50 @@
+#!/usr/bin/env python
+
+# =============================================================================
+# avicaching_data.py
+# Author: Anmol Kabra -- github: @anmolkabra
+# Project: Solving the Avicaching Game Faster and Better (Summer 2017)
+# -----------------------------------------------------------------------------
+# Purpose of the Script:
+#   Reads, writes, mutates and operates on avicaching data files. This module 
+#   is imported in all models for reading and doing things.
+# -----------------------------------------------------------------------------
+# What do terms mean:
+#   X - visit densities before placing rewards (for Identification Problem)
+#   Y - visit densities after placing rewards (for Identification Problem)
+#   R - rewards matrix
+#   J - no. of locations
+#   T - no. of time units
+# -----------------------------------------------------------------------------
+# Required Dependencies/Software:
+#   - Python 2.x (obviously, Anaconda environment used originally)
+#   - NumPy
+# -----------------------------------------------------------------------------
+# Required Local Files/Data/Modules:
+#   - ./data/*
+# =============================================================================
+
 from __future__ import print_function
 import numpy as np
 
 def read_XYR_file(file_name, locs, T):
+    """
+    Read the datafile containing X, Y, R information.
+
+    Args:
+        file_name -- (str) name of the file
+        locs -- (int) J 
+        T -- (int) T
+
+    Returns:
+        3-tuple -- (tuple of NumPy ndarrays) X, Y, R
+    """
     X, Y, R = [], [], []
     with open(file_name, "r") as xyrfile:
         for idx, line in zip(xrange(T * 3), xyrfile):
+            # the line contains locs+ floats separated by spaces
             line_vec = np.array(map(float, line.split()[:locs]))   # only J cols
+            # if reading the first three lines, init the arrays
             if idx == 0:
                 # X init
                 X = line_vec
@@ -16,6 +55,7 @@ def read_XYR_file(file_name, locs, T):
                 # R init
                 R = line_vec
             else:
+                # reading 4+ lines
                 rem = idx % 3
                 if rem == 0:
                     # append X information
@@ -29,23 +69,45 @@ def read_XYR_file(file_name, locs, T):
     return (X, Y, R)
 
 def read_F_file(file_name, locs):
-    F = []
+    """
+    Reads the csv file containing f information.
+
+    Args:
+        file_name -- (str) name of the file
+        locs -- (int) J
+
+    Returns:
+        NumPy ndarray -- f
+    """
+    f = []
     with open(file_name, "r") as fufile:
         next(fufile)        # skip header row of fufile
         for idx, line in zip(xrange(locs), fufile):
-            line_vec = np.array(map(float, line.split(",")[:-3]))  # ignore last 3 cols
+            # ignore last 3 cols which contain lat long information
+            line_vec = np.array(map(float, line.split(",")[:-3]))
             if idx == 0:
                 # F init
-                F = line_vec
+                f = line_vec
             else:
                 # append F info
-                F = np.vstack([F, line_vec])
-    return F
+                f = np.vstack([f, line_vec])
+    return f
 
 def read_dist_file(file_name, locs):
+    """
+    Reads the DIST file containing the distances between all locations.
+
+    Args:
+        file_name -- (str) name of the file
+        locs -- (int) J
+
+    Returns:
+        NumPy ndarray -- DIST
+    """
     DIST = []
     with open(file_name, "r") as distfile:
         for idx, line in zip(xrange(locs), distfile):
+            # only read J rows and cols
             line_vec = np.array(map(float, line.split()))[:locs]
             if idx == 0:
                 # DIST init
@@ -55,49 +117,116 @@ def read_dist_file(file_name, locs):
                 DIST = np.vstack([DIST, line_vec])
     return DIST
 
-def combine_DIST_F(F, DIST, locs, numFeatures):
+def combine_DIST_F(f, DIST, locs, numFeatures):
+    """
+    Combines f and DIST as data preprocessing.
+
+    Args:
+        F -- (NumPy ndarray) f
+        DIST -- (NumPy ndarray) DIST
+        locs -- (int) J
+        numFeatures -- (int) `len(f[i]) + 1` (accounting for the distance element)
+
+    Returns:
+        NumPy ndarray -- represents the input dataset without the rewards.
+    """
     NN_in = np.empty([locs, locs, numFeatures], dtype=float)
-    for v in xrange(len(DIST)):
-        for u in xrange(len(DIST[0])):
+    for v in xrange(locs):
+        for u in xrange(locs):
             NN_in[v][u][0] = DIST[v][u]
-            NN_in[v][u][1:] = F[u]    # last one reserved for rewards
+            NN_in[v][u][1:] = f[u]
     return NN_in
 
 def save_rand_XYR(file_name, X, Y, R, J=116, T=173):
-    # intersperse XYR
+    """
+    Writes X, Y, R information to a file such that it is readable by read_XYR_file().
+    The dimensions of X, Y, R must be J x T each.
+
+    Args:
+        file_name -- (str) name of the file
+        X -- (NumPy ndarray) X
+        Y -- (NumPy ndarray) Y
+        R -- (NumPy ndarray) R
+        J -- (int) no. of locations (default=116)
+        T -- (int) no. of time units (default=173)
+    """
+    # to intersperse XYR
     XYR = np.empty([T * 3, J])
     
     for t in xrange(T):
         if t == 0:
             XYR = X[t]
-            XYR = np.vstack([XYR, Y[t]])
-            XYR = np.vstack([XYR, R[t]])
         else:
             XYR = np.vstack([XYR, X[t]])
-            XYR = np.vstack([XYR, Y[t]])
-            XYR = np.vstack([XYR, R[t]])
+        XYR = np.vstack([XYR, Y[t]])
+        XYR = np.vstack([XYR, R[t]])
     
     np.savetxt(file_name, XYR, fmt="%.8f", delimiter=" ")
 
 def split_along_dim(M, num, dim):
     """
-    Shuffles and splits the matrix M into 2 along dimension 0 at index num, returning
-    two matrices
+    Shuffles and splits a matrix into 2 along a dimension at index, returning
+    two matrices.
+
+    Args:
+        M -- (NumPy ndarray) Matrix to be split
+        num -- (int) index to split at
+        dim -- (int) axis/dimension for splitting
+
+    Returns:
+        2 NumPy ndarrays -- Matrices after splitting
     """
     return np.split(M, [num], axis=dim)
 
 def read_weights_file(file_name, locs_in_file, locs, numFeatures):
+    """
+    Reads the weights file and splits the saved data into 2 weights tensors, 
+    as our models require.
+
+    Args:
+        file_name -- (str) name of the file
+        locs_in_file -- (int) no. of locations used in the weights file
+        locs -- (int) no. of locations required by the run specs 
+            (<= locs_in_file). The function reads the file and ignores data 
+            corresponding to locations > locs
+        numFeatures -- (int) no. of features in the dataset (the run spec 
+            should not request something different than what is in the file). 
+            This also determines the size of the weights tensors
+
+    Returns:
+        2-tuple of NumPy ndarrays -- w1 and w2
+    """
     data = np.loadtxt(file_name)
-    w1, w2 = split_along_dim(data, len(data) - locs_in_file, dim=0)  # w2 is the last locs_in_file rows
-    w1, w2 = w1[:locs * numFeatures], w2[:locs] # take out only locs slices
+    # data is an ndarray with the 1st part representing the 3d w1 (represented as 
+    # 2d slices) and the 2nd part representing w2 - the last locs_in_file rows
+    w1, w2 = split_along_dim(data, len(data) - locs_in_file, dim=0)
+    # take out only locs slices. Since w1 is represented in 2d, this means 
+    # taking out locs * numFeatures slices
+    w1, w2 = w1[:locs * numFeatures], w2[:locs]
     w1 = w1.reshape((locs, numFeatures, numFeatures))
     return (w1, w2)
 
 def read_lat_long_from_Ffile(file_name, locs, lat_col=33, long_col=34):
+    """
+    Reads the latitude and longitude from the file containing f information.
+
+    Args:
+        file_name -- (str) name of the file
+        locs -- (int) no. of locations. Also represents the length of lat and 
+            long vectors
+        lat_col -- (int) col no. in the f file
+        long_col -- (int) col no. in the f file
+
+    Returns:
+        NumPy ndarray -- 2d matrix where the first col are latitudes and the 
+        second col are longitudes
+    """
     lat_long = []
     with open(file_name, "r") as fufile:
         next(fufile)        # skip header row of fufile
         for idx, line in zip(xrange(locs), fufile):
+            # extract latitude and longitude. Since they are stored adjacent, 
+            # just ignore other cols
             line_vec = np.array(map(float, line.split(",")[lat_col:long_col + 1]))
             if idx == 0:
                 # lat_long init
@@ -109,9 +238,20 @@ def read_lat_long_from_Ffile(file_name, locs, lat_col=33, long_col=34):
 
 def normalize(x, along_dim=None, using_max=True, offset_division=0.000001):
     """
-    Normalizes x by dividing each element by the maximum if using_max is True and by the sum
-    if using_max is False. Finding the maximum/sum is specified by along_dim. If along_dim is an int, the max is 
-    calculated along that dimension, if it's None, whole x's max/sum is calculated
+    Normalizes a tensor by dividing each element by the maximum or by the sum, 
+    which are calculated along a dimension. 
+
+    Args:
+        x -- (NumPy ndarray) matrix/tensor to be normalized
+        along_dim -- (int or None) If along_dim is an int, the max is 
+            calculated along that dimension; if it's None, 
+            whole x's max/sum is calculated (default=None)
+        using_max -- (bool) Normalize using max if True and sum if False 
+            (default=True)
+        offset_division -- (float) safety mechanism to avoid division by zero
+
+    Returns:
+        NumPy ndarray -- Normalized matrix/tensor
     """
     if using_max:
         return x / (np.amax(x, axis=along_dim) + offset_division)
@@ -120,8 +260,12 @@ def normalize(x, along_dim=None, using_max=True, offset_division=0.000001):
 
 def make_rand_F_file(file_name, J):
     """
-    I admit this is very bad code. I didn't care about optimizing this data
-    constructor
+    [Extremely bad code. A very bad example of coding style]
+    Creates and write random f file.
+
+    Args:
+        file_name -- (str) name of the file
+        J -- (int) J
     """
     # num visits  -- type random int
     num_visits = np.floor(np.random.rand(J) * 1000)
--- runNNAvicaching_find_weights_rand.sh
+++ runNNAvicaching_find_weights_rand.sh
@@ -7,22 +7,22 @@
 # -----------------------------------------------------------------------------
 # Purpose of the Script:
 #   Script to run multiple versions (diff seeds/features for model) of the 
-#   Identification Problem's model in nnAvicaching_find_weights.py and 
-#   nnAvicaching_find_weights_hiddenlayer.py. GPU Speedup on random datasets.
+#   Identification Problem's model in nnAvicaching_find_weights.py. 
+#   GPU Speedup on random datasets.
 # =============================================================================
 
 # specs for tests:
-# locations: 116
-# batch-size T: 17, 51, 85, 129, 173
+# T: 173
+# batch-size J: 11, 37, 63, 90, 116, 145, 174, 203, 232
 # lr: 0.001
 # epochs: 1000
 
-for T in 17 51 85 129 173
+for J in 11 37 63 90 116 145 174 203 232
 do
     for s in 1 2 3
     do
         # 3-layered model only. time not recorded for the 4-layered model
-        python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time $T --seed $s
-        python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --time $T --seed $s --no-cuda
+        python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --locations $J --seed $s
+        python nnAvicaching_find_weights.py --rand --no-plots --epochs 1000 --locations $J --seed $s --no-cuda
     done
 done
